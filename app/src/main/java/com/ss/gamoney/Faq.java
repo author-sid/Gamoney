b787c9f8bcfde45dc9e7b8f933f3b412c4629f06
author-sid <posttosiddharthsharma@gmail.com>
2020-09-05 02:22:51 +0530

    Register button Bug solved 1

---
+++ app/src/main/java/com/ss/gamoney/Faq.java
@@ -0,0 +1,92 @@
+package com.ss.gamoney;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class Faq extends AppCompatActivity {
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faq);
+        TextView mTitleWindow = (TextView) findViewById(R.id.titleWindow);
+        TextView mMessageWindow= (TextView) findViewById(R.id.messageWindow);
+
+        mTitleWindow.setText("FAQ's");
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        String Message = "How To Join a Tournament:" +
+                "\n " +
+                "\n" +
+                "1.Select the Game from tournament section you want to play.\n" +
+                "2.After selecting Game, Select the tournament type(Different tournament types have different rewards).\n" +
+                "3.Register your team correctly (Any changes after registration is not allowed).\n" +
+                "4.Pay the desired amount.\n" +
+                "5.After successful payment, You will get Match id and password through email and in tournament section, 15 minutes before match.\n" +
+                "6.Rewards will be given just after match through desired payment method.\n" +
+                "\n Q.Will i always get reward?\n" +
+                "A.Yes! if you loose, we will be giving ₹10/kill or depends on tournament type.\n" +
+                "\n" +
+                "Q.Can i play without team?\n" +
+                "A.Yes! You can play without team or team up with random players.\n" +
+                "\n" +
+                "Q.How will i get reward?\n" +
+                "A.You can select your payment option while registering for tournament. \n" +
+                "\n" +
+                "Q.When will rewards be credited? \n" +
+                "A.After the match, Rewards will be transferred immediately with a confirmation email. \n" +
+                "\n" +
+                "Q.If there is a mistake while registering for the tournament? \n" +
+                "A.If you enter wrong name/id of your teammates, You will be disqualified from the match.\n"+
+                "\n" +
+                "*For any queries feel free to contact us: \n ";
+
+        stringBuilder.append(Message);
+        mMessageWindow.setText(stringBuilder.toString());
+
+        //Initialize and assign variable
+        BottomNavigationView bottomNavigationView= findViewById(R.id.bottom_navigation);
+
+        //Set Faq Selected
+        bottomNavigationView.setSelectedItemId(R.id.faq);
+
+        //perform ItemSelectedListener
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                switch(menuItem.getItemId()){
+                    case R.id.tournament:
+                        startActivity(new Intent(getApplicationContext(), Tournaments.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+                    case R.id.profile:
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+                    case R.id.faq:
+                        return true;
+
+                    case R.id.chat:
+                        Toast.makeText(getApplicationContext(),"ComingSoon!",Toast.LENGTH_SHORT).show();
+                        return true;
+
+                }
+                return false;
+            }
+        });
+    }
+}
--- app/src/main/java/com/ss/gamoney/Tournaments.java
+++ app/src/main/java/com/ss/gamoney/Tournaments.java
@@ -1,53 +1,53 @@
 package com.ss.gamoney;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class Tournaments extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //Initialize and assign variable
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        setContentView(R.layout.activity_tournaments);
 
-        //Set Profile Selected
-        bottomNavigationView.setSelectedItemId(R.id.profile);
+        //Initialize and assign variable
+        BottomNavigationView bottomNavigationView= findViewById(R.id.bottom_navigation);
+
+        //Set Tournament Selected
+        bottomNavigationView.setSelectedItemId(R.id.tournament);
 
         //perform ItemSelectedListener
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
 
-                switch (menuItem.getItemId()) {
+                switch(menuItem.getItemId()){
                     case R.id.tournament:
-                        startActivity(new Intent(getApplicationContext(), Tournaments.class));
-                        overridePendingTransition(0, 0);
                         return true;
                     case R.id.profile:
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        overridePendingTransition(0,0);
                         return true;
 
                     case R.id.faq:
-                        startActivity(new Intent(getApplicationContext(), Faq.class));
-                        overridePendingTransition(0, 0);
+                        startActivity(new Intent(getApplicationContext(),Faq.class));
+                        overridePendingTransition(0,0);
                         return true;
 
                     case R.id.chat:
-                        Toast.makeText(getApplicationContext(), "ComingSoon!", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplicationContext(),"ComingSoon!",Toast.LENGTH_LONG).show();
                         return true;
 
                 }
                 return false;
             }
         });
-
     }
 }
