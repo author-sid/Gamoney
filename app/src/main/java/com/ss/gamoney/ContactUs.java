4e9049fa1a03609aa7c88a0f2178a7452686f73c
author-sid <posttosiddharthsharma@gmail.com>
2020-09-12 03:33:59 +0530

    Added contact us backend part

--- app/src/main/java/com/ss/gamoney/ContactUs.java
+++ app/src/main/java/com/ss/gamoney/ContactUs.java
@@ -11,13 +11,19 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ContactUs extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     DrawerLayout drawerLayout;
@@ -35,6 +41,11 @@ public class ContactUs extends AppCompatActivity implements NavigationView.OnNav
         navigationView.bringToFront();
         mAuth = FirebaseAuth.getInstance();
 
+        final EditText your_name = findViewById(R.id.your_name);
+        final EditText your_email = findViewById(R.id.your_email);
+        final EditText your_subject = findViewById(R.id.your_subject);
+        final EditText your_message = findViewById(R.id.your_message);
+
         setSupportActionBar(toolbar);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
@@ -42,7 +53,74 @@ public class ContactUs extends AppCompatActivity implements NavigationView.OnNav
         toggle.syncState();
 
         navigationView.setNavigationItemSelectedListener(this);
+
+        Button email = findViewById(R.id.post_message);
+        email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = your_name.getText().toString();
+                String email = your_email.getText().toString();
+                String subject = your_subject.getText().toString();
+                String message = your_message.getText().toString();
+
+                if (TextUtils.isEmpty(name)){
+                    your_name.setError("Name required");
+                    your_name.requestFocus();
+                    return;
+                }
+
+
+                if (!isValidEmail(email)){
+                    your_email.setError("Invalid email");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(subject)){
+                    your_subject.setError("Enter Your Subject");
+                    your_subject.requestFocus();
+                    return;
+                }
+
+                Intent sendEmail = new Intent(Intent.ACTION_SEND);
+
+                //Fill it with Data
+                sendEmail.setType("plain/text");
+                sendEmail.putExtra(Intent.EXTRA_EMAIL, new String[]{"posttosiddharthsharma@gmail.com"});
+                sendEmail.putExtra(Intent.EXTRA_SUBJECT,subject);
+                sendEmail.putExtra(Intent.EXTRA_TEXT,
+                        "name:" +name + '\n'+"Email ID:" + email + '\n' + "Message:" + '\n' + message );
+
+                //send it off to Activity Chooser
+                startActivity(Intent.createChooser(sendEmail,"Send mail..."));
+            }
+        });
     }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        //Get a Tracker (Should auto-report)
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+    }
+
+    private boolean isValidEmail(String email) {
+        String EMAIL_PATTERN = "^[_A-Za-z0-9-+]+(\\.[_A-Za-z0-9-]+)*@"
+                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
 
     @Override
     public void onBackPressed() {
